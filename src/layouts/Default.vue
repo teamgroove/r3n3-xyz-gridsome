bf92699ab27be4f0f55b2b5363c4cfab43f88d00
René Thiel <thiel@link-innovation.de>
2020-05-01 10:53:23 +0200

    freshen

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,11 +17,12 @@
           & Made with ❤️ by
           <a target="_blank" class="link" href="//alex.design">Alex Brown</a>
         </p>
-      </div>
+      </div>-->
       <div class="footer-links">
-        <a target="_blank" href="/sitemap.xml">Sitemap</a>
-        <a target="_blank" href="/feed.xml">RSS Feed</a>
-      </div> -->
+        <a href="mailto:hello@r3n3.xyz">hello@r3n3.xyz</a>
+        <!-- <a target="_blank" href="/sitemap.xml">Sitemap</a>
+        <a target="_blank" href="/feed.xml">RSS Feed</a> -->
+      </div>
     </div>
   </main>
 </template>
